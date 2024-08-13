6217005cd2dc94297cfbf5a35220d12cd9fc6a87
bulkaSMakom16 <slefkubi@gmail.com>
2024-08-13 23:42:41 +0300

    Base Template/Static setup

--- itstep/web/views.py
+++ itstep/web/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    context = {}
+    return render(request, 'web/index.html', context)
+
+def about(request):
+    context = {}
+    return render(request,'web/about.html', context)
+
+def services(request):
+    context = {}
+    return render(request, 'web/services.html', context)
+
+def blog(request):
+    context = {}
+    return render(request, 'web/blog.html', context)
+
+def contact(request):
+    context = {}
+    return render(request, 'web/contact.html', context)
